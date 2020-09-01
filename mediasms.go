5ebcb0f11c73205249eb5cc0b079d42de5ff3227
Christopher <gsyme1907@gmail.com>
2020-09-01 14:01:21 +0900

    feat: POST and GET

--- mediasms.go
+++ mediasms.go
@@ -1,12 +1,12 @@
-package mediasms
+package mediatest
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
-	"strings"
 )
 
 // Client struct
@@ -17,41 +17,58 @@ type Client struct {
 }
 
 // Send request to media4u
-func (c Client) Send(messageID string) {
+func (c Client) Send(messageID string) (string, error) {
 	smsID := createSMSID(c.AccountID, messageID)
 
-	form := url.Values{
-		"smsid":         {smsID},
-		"mobilenumber":  {"07043459253"},
-		"status":        {"1"},
-		"type":          {"sms"},
-		"returnsms":     {"1"},
-		"waitreturnsms": {"1"},
-		"smstext":       {"Click Here 17 {URL}"},
-		"originalurl":   {"https://google.com"},
+	values := map[string]string{
+		"smstext":      "Click Here 4 {URL}",
+		"mobilenumber": "07043459253",
+		"originalurl":  "https://google.com",
+		"smsid":        smsID,
+		"status":       "1",
+		"type":         "sms",
 	}
 
-	// requestBody, err := json.Marshal(map[string]string{
-	// 	"mobilenumber":  "07043459253",
-	// 	"smstext":       "Click Here 17 {URL}",
-	// 	"originalurl":   "https://google.com",
-	// 	"smsid":         smsID,
-	// 	"status":        "1",
-	// 	"type":          "sms",
-	// 	"returnsms":     "1",
-	// 	"waitreturnsms": "1",
-	// })
+	jsonValue, _ := json.Marshal(values)
 
 	client := &http.Client{}
-	req, err := http.NewRequest("POST", "https://www.sms-console.jp/api/", strings.NewReader(form.Encode()))
+	req, err := http.NewRequest("POST", "https://www.sms-console.jp/api/", bytes.NewBuffer(jsonValue))
 	req.SetBasicAuth(c.Username, c.Password)
+	req.Header.Set("Content-Type", "application/json")
+	fmt.Println(req)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	defer resp.Body.Close()
+
 	bodyText, err := ioutil.ReadAll(resp.Body)
 	s := string(bodyText)
-	fmt.Println(s)
+	return s, err
+}
+
+// GetStatus of a sent sms
+func (c Client) GetStatus(messageID string) (string, error) {
+	smsID := createSMSID(c.AccountID, messageID)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://www.sms-console.jp/api5/?smsid="+smsID, nil)
+	req.SetBasicAuth(c.Username, c.Password)
+	req.Header.Set("Content-Type", "application/json")
+	fmt.Println(req)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	s := string(bodyText)
+
+	return s, err
+
 }
 
 func createSMSID(accountID, messageID string) string {
