0637a3d7a6e2cb6a07a601125b7f29a371e660e2
Anderson Oki <anderson@comvex.co.jp>
2020-11-13 11:46:49 +0900

    Add validation

--- models/build_request.go
+++ models/build_request.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"encoding/json"
+	"errors"
 	"strconv"
 	"strings"
 )
@@ -18,8 +19,8 @@ type BuildRequest struct {
 	OriginalURL3  string
 	OriginalURL4  string
 	Status        string
-	ReturnSMS     string
-	WaitReturnSMS string
+	ReturnSMS     bool
+	WaitReturnSMS bool
 	Type          string
 	Au            AuConfig
 	Docomo        DocomoConfig
@@ -43,8 +44,14 @@ func NewBuildRequest() BuildRequest {
 
 // MarshalJSON overrides the custom serialization to prepare the data for the MediaSms API
 func (br BuildRequest) MarshalJSON() ([]byte, error) {
+	err := br.validate()
+
+	if err != nil {
+		return nil, err
+	}
+
 	return json.Marshal(&struct {
-		SMSID         string `json:"smsid,omitempty"`
+		SMSID         string `json:"smsid,omitempty"` // SMSID is required when the ReturnSMS is true
 		SMSTitle      string `json:"smstitle,omitempty"`
 		SMSText       string `json:"smstext"`
 		OriginalURL   string `json:"originalurl,omitempty"`
@@ -72,19 +79,55 @@ func (br BuildRequest) MarshalJSON() ([]byte, error) {
 		OriginalURL4:  br.OriginalURL4,
 		MobileNumber:  br.MobileNumber,
 		Status:        br.Status,
-		ReturnSMS:     br.ReturnSMS,
-		WaitReturnSMS: br.WaitReturnSMS,
+		ReturnSMS:     buildBooleanPayload(br.ReturnSMS),
+		WaitReturnSMS: buildBooleanPayload(br.WaitReturnSMS),
 		Type:          br.Type,
 		Au:            strconv.Itoa(br.Au.NumberOfRetries),
-		Docomo:        buildRetries(br.Docomo.NumberOfRetries, br.Docomo.RetryInterval),
-		Softbank:      buildRetries(br.Softbank.NumberOfRetries, br.Softbank.RetryInterval),
-		Gateway:       buildRetries(br.Gateway.NumberOfRetries, br.Gateway.RetryInterval),
-		Rakuten:       buildRetries(br.Rakuten.NumberOfRetries, br.Rakuten.RetryInterval),
+		Docomo:        buildRetriesPayload(br.Docomo.NumberOfRetries, br.Docomo.RetryInterval),
+		Softbank:      buildRetriesPayload(br.Softbank.NumberOfRetries, br.Softbank.RetryInterval),
+		Gateway:       buildRetriesPayload(br.Gateway.NumberOfRetries, br.Gateway.RetryInterval),
+		Rakuten:       buildRetriesPayload(br.Rakuten.NumberOfRetries, br.Rakuten.RetryInterval),
 		Sim:           strconv.Itoa(br.Sim.NumberOfRetries),
 	})
 }
 
-func buildRetries(numberOfRetries int, interval []int) string {
+func (br BuildRequest) validate() error {
+	if br.ReturnSMS && br.SMSID == "" {
+		return errors.New("SMSID is required on two way sms (ReturnSMS) is enabled")
+	}
+
+	if len(br.Docomo.RetryInterval) != br.Docomo.NumberOfRetries {
+		return errors.New("[Docomo] Invalid amount of retries interval values, the number of retries interval has to be the same as number of retries specified")
+	}
+
+	if len(br.Softbank.RetryInterval) != br.Softbank.NumberOfRetries {
+		return errors.New("[Softbank] Invalid amount of retries interval values, the number of retries interval has to be the same as number of retries specified")
+	}
+
+	if len(br.Gateway.RetryInterval) != br.Gateway.NumberOfRetries {
+		return errors.New("[Gateway] Invalid amount of retries interval values, the number of retries interval has to be the same as number of retries specified")
+	}
+
+	if len(br.Rakuten.RetryInterval) != br.Rakuten.NumberOfRetries {
+		return errors.New("[Rakuten] Invalid amount of retries interval values, the number of retries interval has to be the same as number of retries specified")
+	}
+
+	if len(br.Sim.RetryInterval) != br.Sim.NumberOfRetries {
+		return errors.New("[Sim] Invalid amount of retries interval values, the number of retries interval has to be the same as number of retries specified")
+	}
+
+	return nil
+}
+
+func buildBooleanPayload(val bool) string {
+	if val {
+		return "1"
+	}
+
+	return "0"
+}
+
+func buildRetriesPayload(numberOfRetries int, interval []int) string {
 	var sb strings.Builder
 
 	sb.WriteString(strconv.Itoa(numberOfRetries))
--- models/build_request_test.go
+++ models/build_request_test.go
@@ -11,9 +11,13 @@ func TestBuildRequestMarshalJson(t *testing.T) {
 	br.MobileNumber = "07012345678"
 	br.SMSText = "Lorem Ipsum"
 
-	mbr, _ := json.Marshal(br)
+	mbr, err := json.Marshal(br)
 
-	expectedJSONString := `{"smstext":"Lorem Ipsum","mobilenumber":"07012345678","au":"1","docomo":"1-0","softbank":"1-5","gateway":"1-1","rakuten":"1-2","sim":"1"}`
+	if err != nil {
+		t.Errorf("Validation Failure %s", err.Error())
+	}
+
+	expectedJSONString := `{"smstext":"Lorem Ipsum","mobilenumber":"07012345678","returnsms":"0","waitreturnsms":"0","au":"1","docomo":"1-0","softbank":"1-5","gateway":"1-1","rakuten":"1-2","sim":"1"}`
 
 	if string(mbr) != expectedJSONString {
 		t.Errorf("Unexpected Serialization Value %s", string(mbr))
