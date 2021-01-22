781c239ace6ba57ea9b101565d9a7566a87a0ea0
Anderson Oki <anderson@comvex.co.jp>
2021-01-22 13:20:28 +0900

    WIP

--- errors/results_mapper.go
+++ errors/results_mapper.go
@@ -3,291 +3,291 @@ package errors
 // ResultsMapper is a hashmap of all possible results when sending an sms
 var ResultsMapper = map[string]map[string]string{
 	"200": {
-		"name":        "success_code",
-		"description": "Sent SMS",
+		"name":        Success,
+		"description": "Message has been succesfully sent",
 	},
 	"401": {
-		"name":        "authentication_error",
-		"description": "Authorization Required",
+		"name":        AuthenticationError,
+		"description": "Authorization required",
 	},
 	"405": {
-		"name":        "method",
-		"description": "405 Method not allowed",
+		"name":        MethodNotAllowed,
+		"description": "Method not allowed",
 	},
 	"414": {
-		"name":        "failed_to_send_request",
-		"description": "URL is longer than 8,190 byte",
+		"name":        URLTooLong,
+		"description": "URL is longer than 8,190 bytes",
 	},
 	"502": {
-		"name":        "bad_gateway",
+		"name":        BadGateway,
 		"description": "SMS-CONSOLE completely stopped",
 	},
 	"503": {
-		"name":        "temporarily_unavailable",
+		"name":        TemporaryUnavailable,
 		"description": "User reached max limit on requests/sec from the single IP address",
 	},
 	"514": {
-		"name":        "sms_doesnt_exist",
+		"name":        SMSNotFound,
 		"description": "SMS does not exist",
 	},
 	"550": {
-		"name":        "failed_to_send_sms",
+		"name":        SendFailure,
 		"description": "Failed to send SMS",
 	},
 	"555": {
-		"name":        "block_ip_address",
+		"name":        BlockedIPAddress,
 		"description": "Your IP address has been blocked",
 	},
 	"560": {
-		"name":        "mobile_number",
+		"name":        InvalidMobileNumber,
 		"description": "Mobile number is invalid",
 	},
 	"562": {
-		"name":        "send_date_&_time",
+		"name":        InvalidSendDateTime,
 		"description": "Send date & time is invalid.",
 	},
 	"564": {
-		"name":        "reply_id_is_invalid",
+		"name":        InvalidReplyID,
 		"description": "Reply ID is invalid.",
 	},
 	"566": {
-		"name":        "reply_id_doesn't_exist",
+		"name":        ReplyIDNotFound,
 		"description": "Reply ID doesn't exist.",
 	},
 	"568": {
-		"name":        "au_title",
+		"name":        InvalidAuTitle,
 		"description": "AU SMS title is invalid",
 	},
 	"569": {
-		"name":        "softbank_title",
+		"name":        InvalidSoftbankTitle,
 		"description": "Softbank SMS title is invalid",
 	},
 	"570": {
-		"name":        "basic_sms_text_id",
+		"name":        InvalidSMSTextID,
 		"description": "basic SMS text ID is invalid",
 	},
 	"571": {
-		"name":        "sending_attempts_is_invalid",
+		"name":        InvalidSendingAttempts,
 		"description": "Sending attempts is invalid.",
 	},
 	"572": {
-		"name":        "resending_interval_is_invalid",
+		"name":        InvalidResendingInterval,
 		"description": "Resending interval is invalid.",
 	},
 	"573": {
-		"name":        "status",
+		"name":        InvalidStatus,
 		"description": "Status is invalid",
 	},
 	"574": {
-		"name":        "sms_id",
+		"name":        InvalidSMSID,
 		"description": "SMS ID is invalid",
 	},
 	"575": {
-		"name":        "docomo",
+		"name":        InvalidDocomoTTL,
 		"description": "Docomo is invalid",
 	},
 	"576": {
-		"name":        "au",
+		"name":        InvalidAuTTL,
 		"description": "au is invalid",
 	},
 	"577": {
-		"name":        "soft_bank",
+		"name":        InvalidSoftbankTTL,
 		"description": "Soft Bank is invalid",
 	},
 	"579": {
-		"name":        "gateway",
+		"name":        InvalidGatewayTTL,
 		"description": "Gateway is invalid",
 	},
 	"580": {
-		"name":        "sms_title",
+		"name":        InvalidSMSTitle,
 		"description": "SMS title is invalid",
 	},
 	"585": {
-		"name":        "basic_sms_text",
+		"name":        InvalidBasicSMSText,
 		"description": "Basic SMS text is invalid",
 	},
 	"586": {
-		"name":        "sms_text_2_(forSDP)",
+		"name":        InvalidSMSTextTwo,
 		"description": "SMS text 2 is invalid",
 	},
 	"587": {
-		"name":        "not_unique_sms_id",
+		"name":        SMSIDNotUnique,
 		"description": "SMS ID is not unique",
 	},
 	"588": {
-		"name":        "sms_text_for_docomo",
+		"name":        InvalidDocomoSMSText,
 		"description": "SMS text for Docomo is invalid",
 	},
 	"589": {
-		"name":        "sms_text_for_softbank",
+		"name":        InvalidSoftbankSMSText,
 		"description": "SMS text for Softbank is invalid",
 	},
 	"590": {
-		"name":        "original_url",
+		"name":        InvalidOriginalURL,
 		"description": "Original URL is invalid",
 	},
 	"591": {
-		"name":        "sms_text_is_disabled",
+		"name":        DisabledSMSText,
 		"description": "SMS text is disabled",
 	},
 	"592": {
-		"name":        "time_is_disabled",
-		"description": "Time is disabled.",
+		"name":        InvalidDispatchTime,
+		"description": "Dispatch time is not on the allowed range.",
 	},
 	"593": {
-		"name":        "sms",
+		"name":        InvalidWaitReturnSMS,
 		"description": "Wait return SMS is invalid",
 	},
 	"594": {
-		"name":        "return_sms",
+		"name":        InvalidReturnSMS,
 		"description": "Return SMS is invalid",
 	},
 	"595": {
-		"name":        "2_way_sms_function_is_invalid",
+		"name":        InvalidTwoWaySmsFunction,
 		"description": "2-way SMS is disabled",
 	},
 	"598": {
-		"name":        "docomo_title",
+		"name":        InvalidDocomoTitle,
 		"description": "Docomo SMS title is invalid",
 	},
 	"599": {
-		"name":        "resending_is_disabled",
+		"name":        DisabledResending,
 		"description": "Resending is disabled.",
 	},
 	"600": {
-		"name":        "keep_chatid",
+		"name":        InvalidKeepChatID,
 		"description": "Keep chatid is invalid",
 	},
 	"601": {
-		"name":        "sms_title_function_is_disabled",
+		"name":        DisabledSMSTitleFunction,
 		"description": "SMS title function is disabled",
 	},
 	"602": {
-		"name":        "sim_title",
+		"name":        InvalidSIMTitle,
 		"description": "SIM SMS title is invalid.",
 	},
 	"603": {
-		"name":        "reminder_delay",
+		"name":        InvalidReminderDelay,
 		"description": "Reminder delay is invalid",
 	},
 	"604": {
-		"name":        "reminder_text",
+		"name":        InvalidReminderText,
 		"description": "Reminder text is invalid",
 	},
 	"605": {
-		"name":        "type",
+		"name":        InvalidType,
 		"description": "Invalid type",
 	},
 	"606": {
-		"name":        "3rd_party_api_is_disabled",
+		"name":        DisabledThirdPartyAPI,
 		"description": "3rd party API is disabled",
 	},
 	"608": {
-		"name":        "registration_date",
+		"name":        InvalidRegistrationDate,
 		"description": "Invalid Registration date",
 	},
 	"610": {
-		"name":        "hlr_is_disabled",
+		"name":        DisabledHLR,
 		"description": "HLR is disabled",
 	},
 	"612": {
-		"name":        "original_url2",
+		"name":        InvalidOriginalURLTwo,
 		"description": "612 Original URL 2 is invalid",
 	},
 	"613": {
-		"name":        "original_url3",
+		"name":        InvalidOriginalURLThree,
 		"description": "613 Original URL 3 is invalid",
 	},
 	"614": {
-		"name":        "original_url4",
+		"name":        InvalidOriginalURLFour,
 		"description": "614 Original URL 4 is invalid",
 	},
 	"615": {
-		"name":        "json_format_is_incorrect",
+		"name":        InvalidJSONFormat,
 		"description": "615 Incorrect JSON format",
 	},
 	"617": {
-		"name":        "memo_is_disabled",
+		"name":        DisabledMemo,
 		"description": "Memo is disabled",
 	},
 	"618": {
-		"name":        "basic_sms_text_is_long_for_docomo_api",
+		"name":        DocomoBasicSMSTextTooLong,
 		"description": "618 SMS text is long for Docomo API",
 	},
 	"619": {
-		"name":        "basic_sms_text_is_long_for_sdp",
+		"name":        SDPBasicSMSTextTooLong,
 		"description": "619 SMS text is long for SDP",
 	},
 	"620": {
-		"name":        "basic_sms_text_is_long_for_softbank_api",
+		"name":        SoftbankSMSTextTooLong,
 		"description": "620 SMS text is long for Softbank API",
 	},
 	"621": {
-		"name":        "reminder_text_is_long_for_docomo_api",
+		"name":        DocomoReminderTextTooLong,
 		"description": "Reminder text is long for Docomo API",
 	},
 	"622": {
-		"name":        "reminder_text_is_long_for_sdp",
+		"name":        SDPReminderTextTooLong,
 		"description": "Reminder text is long for SDP",
 	},
 	"623": {
-		"name":        "reminder_text_is_long_for_softbank_api",
+		"name":        SoftbankReminderTextTooLong,
 		"description": "Reminder text is long for Softbank API",
 	},
 	"624": {
-		"name":        "duplicated_smsid",
+		"name":        DuplicatedSMSId,
 		"description": "Duplicated SMSID",
 	},
 	"631": {
-		"name":        "resending_parameters_can_not_be_changed",
+		"name":        ResendingParametersChangeFailure,
 		"description": "Resending parameters can not be changed",
 	},
 	"632": {
-		"name":        "rakuten_title_is_invalid",
+		"name":        InvalidRakutenTitle,
 		"description": "Rakuten title is invalid",
 	},
 	"633": {
-		"name":        "rakuten_text_is_invalid",
+		"name":        InvalidRakutenText,
 		"description": "Rakuten text is invalid",
 	},
 	"634": {
-		"name":        "main_text_is_too_long_for_rakuten",
+		"name":        RakutenMainTextTooLong,
 		"description": "Main text is too long for Rakuten",
 	},
 	"635": {
-		"name":        "reminder_text_is_too_long_for_rakuten",
+		"name":        RakutenReminderTextTooLong,
 		"description": "Reminder text is too long for Rakuten",
 	},
 	"636": {
-		"name":        "rakuten_is_invalid",
+		"name":        InvalidRakutenTTL,
 		"description": "Rakuten is invalid",
 	},
 	"637": {
-		"name":        "rcs_is_disabled",
+		"name":        DisabledRCS,
 		"description": "RCS is disabled",
 	},
 	"639": {
-		"name":        "original_url_code_is_disabled",
+		"name":        DisabledOriginalURLCode,
 		"description": "639 Original URL code is disabled",
 	},
 	"640": {
-		"name":        "original_url_code",
+		"name":        InvalidOriginalURLCode,
 		"description": "640 Original URL code is invalid",
 	},
 	"641": {
-		"name":        "original_url_code_2",
+		"name":        InvalidOriginalURLCodeTwo,
 		"description": "641 Original URL code 2 is invalid",
 	},
 	"642": {
-		"name":        "original_url_code_3",
+		"name":        InvalidOriginalURLCodeThree,
 		"description": "642 Original URL code 3 is invalid",
 	},
 	"643": {
-		"name":        "original_url_code_4",
+		"name":        InvalidOriginalURLCodeFour,
 		"description": "643 Original URL code 4 is invalid",
 	},
 	"666": {
-		"name":        "prior_to_block_ip_address",
+		"name":        IPAddressPriorBlock,
 		"description": "Prior to block IP address",
 	},
 }
