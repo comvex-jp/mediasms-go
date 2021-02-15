241181392bad90f13d1748f19fe1db17057695c8
johncomvex <john@comvex.co.jp>
2021-02-15 16:48:45 +0900

    Add mock testing

--- mediasms_test.go
+++ mediasms_test.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/comvex-jp/mediasms-go"
 	"github.com/comvex-jp/mediasms-go/models"
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/assert"
@@ -15,7 +14,7 @@ func TestReplaceMessageBodyURLs(t *testing.T) {
 	messageBody := "http://google.com to https://github.com to http://stackoverflow.com and http://google.com"
 	allURLs := []string{"http://google.com", "https://github.com", "http://stackoverflow.com", "http://google.com"}
 
-	newMessageBody := mediasms.ReplaceMessageBodyURLs(messageBody, allURLs)
+	newMessageBody := ReplaceMessageBodyURLs(messageBody, allURLs)
 	assert.Equal(t, "{URL} to {URL2} to {URL3} and {URL4}", newMessageBody, "they should be equal")
 }
 
@@ -24,7 +23,7 @@ func TestMediaSMSResponder(t *testing.T) {
 		assert.Fail(t, "Error setting env variable: ", err)
 	}
 
-	client := mediasms.Client{
+	client := Client{
 		Username: "dgm_sms_test_1",
 		Password: "password",
 		Prefix:   "A1",
@@ -75,7 +74,7 @@ func TestMediaSMSResponder(t *testing.T) {
 	defer httpmock.DeactivateAndReset()
 
 	// Mock responders
-	httpmock.RegisterResponder("POST", mediasms.SMSURL+"api/",
+	httpmock.RegisterResponder("POST", SMSURL+"api/",
 		httpmock.NewStringResponder(200, string(jsonResponse)))
 
 	mResponse, err := client.Send("M1", request)
